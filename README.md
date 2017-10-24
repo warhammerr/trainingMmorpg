0ede4ee9e15813949c54dce109bada6ed7670ff9
Dmitry Shutov <7dmitry77@gmail.com>
2017-10-24 09:11:26 +0400

    Немного правок

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # trainingMmorpg
 Training JS skils on MMORPG-browser game
 Base code from https://habrahabr.ru/post/127525/
+
+ Для развертывания приложения в public и server надо сделать npm install
+ 
+ Сервер запускается командой npm run dev
--- public/client.js
+++ public/client.js
@@ -1,5 +1,8 @@
+const SERVER_URL = 'http://localhost:8080';
+
 // Создаем текст сообщений для событий
-strings = {
+// TODO заменить хотя бы на ES6 string literal
+const strings = {
 	'connected': '[sys][time]%time%[/time]: Вы успешно соединились к сервером как [user]%name%[/user].[/sys]',
 	'userJoined': '[sys][time]%time%[/time]: Пользователь [user]%name%[/user] присоединился к чату.[/sys]',
 	'messageSent': '[out][time]%time%[/time]: [user]%name%[/user]: %text%[/out]',
@@ -7,13 +10,11 @@ strings = {
 	'userSplit': '[sys][time]%time%[/time]: Пользователь [user]%name%[/user] покинул чат.[/sys]'
 };
 window.onload = function() {
-	// Создаем соединение с сервером; websockets почему-то в Хроме не работают, используем xhr
-	if (navigator.userAgent.toLowerCase().indexOf('chrome') != -1) {
-		socket = io.connect('http://127.0.0.1:8080', {'transports': ['xhr-polling']});
-	} else {
-		socket = io.connect('http://127.0.0.1:8080');
-	}
+
+	const socket = io.connect(SERVER_URL);
+
 	socket.on('connect', function () {
+
 		socket.on('message', function (msg) {
 			// Добавляем в лог сообщение, заменив время, имя и текст на полученные
 			document.querySelector('#log').innerHTML += strings[msg.event].replace(/\[([a-z]+)\]/g, '<span class="$1">').replace(/\[\/[a-z]+\]/g, '</span>').replace(/\%time\%/, msg.time).replace(/\%name\%/, msg.name).replace(/\%text\%/, unescape(msg.text).replace('<', '&lt;').replace('>', '&gt;')) + '<br>';
@@ -21,17 +22,20 @@ window.onload = function() {
 			document.querySelector('#log').scrollTop = document.querySelector('#log').scrollHeight;
 		});
 		// При нажатии <Enter> или кнопки отправляем текст
-		document.querySelector('#input').onkeypress = function(e) {
-			if (e.which == '13') {
-				// Отправляем содержимое input'а, закодированное в escape-последовательность
-				socket.send(escape(document.querySelector('#input').value));
-				// Очищаем input
-				document.querySelector('#input').value = '';
-			}
+
+    document.querySelector('#input').onkeypress = function(e) {
+		  sendMessage();
 		};
-		document.querySelector('#send').onclick = function() {
-			socket.send(escape(document.querySelector('#input').value));
-			document.querySelector('#input').value = '';
-		};		
+
+    document.querySelector('#send').onclick = function() {
+      sendMessage();
+		};
+
+    function sendMessage() {
+      // TODO найти, придумать sanitizer. Нужен ли он?
+      const message = document.querySelector('#input').value;
+      socket.send(message);
+      document.querySelector('#input').value = '';
+    }
 	});
 };
